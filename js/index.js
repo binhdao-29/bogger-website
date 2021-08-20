ad36d885f920730d226d643e00d667fbf6400483
binhdao29 <binhchan0293@gmail.com>
2021-08-20 16:30:48 +0700

    feat: blog carousel

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+/**
+ * Navigation
+ */
+
 const nav = document.querySelector('.nav');
 const navIcon = document.querySelector('.nav .nav-icons')
 
@@ -6,7 +10,11 @@ navIcon.addEventListener('click', () => {
 });
 
 
-// ----------Swiper-------------
+/**
+ * Swiper
+ */
+
+// ----------Hero---------------------
 const swiper = new Swiper(".hero", {
   effect: "fade",
   grabCursor: "true",
@@ -17,4 +25,26 @@ const swiper = new Swiper(".hero", {
   autoplay: {
     delay: 2500
   },
+});
+
+// ----------Blog Carousel-------------
+const swiperBlog = new Swiper(".blog-carousel", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 768
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+  }
 });
